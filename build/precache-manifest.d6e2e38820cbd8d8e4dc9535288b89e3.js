024b89d78a106061067484acdb1a6b1e066c6ff8
ThahaShahzad <mw3boy8thstrox@gmail.com>
2020-03-30 00:41:24 -0500

    build 3/30 12:40 AM

---
+++ build/precache-manifest.d6e2e38820cbd8d8e4dc9535288b89e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "041f88342b4b0d40663cc7422a2ee7ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15b7dcb9695e68e72047",
+    "url": "/static/css/main.aee3b348.chunk.css"
+  },
+  {
+    "revision": "a6cfcfa679f0ee10bce3",
+    "url": "/static/js/2.48c145b4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.48c145b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15b7dcb9695e68e72047",
+    "url": "/static/js/main.a61098db.chunk.js"
+  },
+  {
+    "revision": "4ccdf9d30b7d9123bc13",
+    "url": "/static/js/runtime-main.56779f6e.js"
+  }
+]);
